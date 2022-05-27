45b6b28fc5d6be7bc8426cf456571c679a7777d9
Barbyvel <barbyvel@yahoo.com.ar>
2022-05-26 23:54:25 -0300

    nuevo form

--- app.js
+++ app.js
@@ -1,184 +1,124 @@
-(function() {
-	"use strict";
-	var //GLOBAL VARIABLES
-	input,
-			container,
-			//CSS CLASSES
-			classSuccess = "success",
-			classError = "error",
-			//FORM VALIDATOR
-			formValidator = {
-				init: function() {
-					this.cacheDom();
-					this.bindEvents();
-				},
-				cacheDom: function() {
-					//MAIN PARENT ELEMENT
-					this.contactForm = document.getElementById("contactForm");
-					//MAIN FORM ELEMENTS
-					this.formHeader = document.querySelector("#formHeader h1");
-					this.formBody = document.getElementById("formBody");
-					this.inputContainer = document.getElementsByClassName("inputContainer");
-					//USER INPUT ELEMENTS
-					//INPUT FIELDS
-					this.fields = {
-						userName: document.getElementById("userName"),
-						userEmail: document.getElementById("userEmail"),
-						userMessage: document.getElementById("userMessage")
-					};
-					this.submitBtn = document.getElementById("submitBtn");
-				},
-				bindEvents: function() {
-					var i;
-					//RUN RULES ON SUBMIT BUTTON CLICK
-					this.submitBtn.onclick = this.runRules.bind(this);
-					//BIND EVENTS TO EACH INPUT FIELD
-					for (i in this.fields) {
-						if (this.fields.hasOwnProperty(i)) {
-							//VARIABLES
-							input = this.fields[i];
-							container = input.parentElement;
-							//RUN RULES WHEN INPUT HAS FOCUS
-							input.onfocus = this.runRules.bind(this);
-							//RESET ERRORS WHEN CONTAINER IS CLICKED
-							container.onclick = this.resetErrors.bind(this, input);
-						}
-					}
-				},
-				runRules: function(evnt) {
-					var target = evnt.target,
-							type = evnt.type;
-					//IF EVENT ON SUBMIT BUTTON
-					if (target === this.submitBtn) {
-						//PREVENT FORM SUBMITTION
-						this.preventDefault(evnt);
-						//IF INPUT HAS FOCUS
-					} else if (type === "focus") {
-						//RESET CLASSLIST
-						this.resetClassList(target.parentElement);
-						//RESET ERRORS
-						this.resetErrors(target);
-						return false;
-					}
-					//RESET CLASSLIST
-					this.resetClassList();
-					//CHECK FIELDS
-					this.checkFields();
-				},
-				preventDefault: function(evnt) {
-					//PREVENT DEFUALT
-					evnt.preventDefault();
-				},
-				checkFields: function() {
-					var i,
-							validCount = 0,
-							//EMAIL FILTER 
-							filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-					//CYLCE THROUGH INPUTS
-					for (i in this.fields) {
-						if (this.fields.hasOwnProperty(i)) {
-							input = this.fields[i];
-							//CHECK IF FIELD IS EMPTY
-							if (input.value === "") {
-								//ADD ERROR CLASS
-								this.addClass(input, classError);
-								//CHECK IF EMAIL IS VALID
-							} else if (i === "userEmail" && !filter.test(input.value)) {
-								//ADD ERROR CLASS
-								this.addClass(input, classError);
-							} else {
-								//FIELD IS VALID
-								this.addClass(input, classSuccess);
-								validCount += 1;
-							}
-						}
-					}
-					//IF ALL FEILDS ARE VALID
-					if (validCount === 3) {
-						//SUBMIT FORM
-						this.submitForm();
-					}
-				},
-				addClass: function(input, clss) {
-					container = input.parentElement;
-					//IF INPUT HAS ERROR
-					if (clss === classError) {
-						//SHOW ERROR MESSAGE
-						this.errorMessage(input);
-					}
-					//ADD CLASS
-					input.parentElement.classList.add(clss);
-				},
-				errorMessage: function(input) {
-					var message;
-					//IF USERNAME HAS ERROR
-					if (input === this.fields.userName) {
-						message = "Ingresa tu nombre";
-						//ELSE IF USEREMAIL HAS ERROR 
-					} else if (input === this.fields.userEmail) {
-						message = "Ingresa un email válido";
-						//ELSE IF USERMESSAGE HAS ERROR
-					} else if (input === this.fields.userMessage) {
-						message = "Ingresa tus comentarios";
-					}
-					this.renderError(input, message);
-				},
-				renderError: function(input, message) {
-					var html;
-					//GET INPUT CONTAINER
-					container = input.parentElement;
-					//RENDER HTML
-					html = document.createElement("div");
-					html.setAttribute("class", "message");
-					html.innerHTML = message;
-					//IF MESSAGE ELEMENT DOESN'T EXIST
-					if (!container.getElementsByClassName("message")[0]) {
-						//INSERT MESSAGE TO INPUT CONTAINER
-						container.insertBefore(html, container.firstElementChild);
-					}
-				},
-				resetClassList: function(input) {
-					var i;
-					//IF TARGETING SPECIFIC INPUT
-					if (input) {
-						//GET INPUT CONTAINER
-						container = input.parentElement;
-						//REMOVE CLASSES
-						container.classList.remove(classError, classSuccess);
-						//FOCUS ON INPUT FIELD
-						input.focus();
-					} else {
-						for (i in this.fields) {
-							if (this.fields.hasOwnProperty(i)) {
-								//REMOVE CLASSES FROM ALL FIELDS
-								this.fields[i].parentElement.classList.remove(classError, classSuccess);
-							}
-						}
-					}
-				},
-				resetErrors: function(input) {
-					//GET INPUT CONTAINER
-					container = input.parentElement;
-					//IF CONTAINER CONTAINS ERROR
-					if (container.classList.contains(classError)) {
-						//RESET CLASSES
-						this.resetClassList(input);
-					}
-				},
-				submitForm: function() {
-					var waitForAnimation;
-					//ADD SUCCESS CLASS
-					this.contactForm.classList.add(classSuccess);
-					//WAIT FOR ANIMATION TO FINISH
-					this.changeHeader("Envío exitoso");
-					//WAIT FOR ANIMATION TO FINISH
-					setTimeout(this.changeHeader.bind(this, "Gracias por escribirnos"), 1200);
-				},
-				changeHeader: function(text) {
-					//CHANGE HEADER TEXT
-					this.formHeader.innerHTML = text;
-				}
-			};
-	//INITIATE FORM VALIDATOR
-	formValidator.init();
-}());
+const usernameEl = document.querySelector('#username');
+const emailEl = document.querySelector('#email');
+
+
+const form = document.querySelector('#signup');
+
+const checkUsername = () => {
+
+    let valid = false;
+
+    const min = 3,
+        max = 25;
+
+    const username = usernameEl.value.trim();
+
+    if (!isRequired(username)) {
+        showError(usernameEl, 'Nombre requerido');
+    } else if (!isBetween(username.length, min, max)) {
+        showError(usernameEl, `El nombre debe de tener entre ${min} y ${max} caracteres`)
+    } else {
+        showSuccess(usernameEl);
+        valid = true;
+    }
+    return valid;
+};
+
+
+
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email requerido');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email no válido')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+};
+
+
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+
+const isRequired = value => value === '' ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+
+const showError = (input, message) => {
+    // get the form-field element
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    // show the error message
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input.parentElement;
+
+    // remove the error class
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    // hide the error message
+    const error = formField.querySelector('small');
+    error.textContent = '';
+}
+
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault();
+
+    // validate forms
+    let isUsernameValid = checkUsername(),
+        isEmailValid = checkEmail();
+       
+
+    let isFormValid = isUsernameValid &&
+        isEmailValid;
+      
+
+    // submit to the server if the form is valid
+    if (isFormValid) {
+        form.submit();
+        
+    }
+});
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+// 
+form.addEventListener('input',function (e) {
+    switch (e.target.id) {
+        case 'username':
+            checkUsername();
+            break;
+        case 'email':
+            checkEmail();
+            break;
+        
+    }
+});
