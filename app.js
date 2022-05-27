64c001c5269ccbb01300f7d860e459004ecf8908
Barbyvel <barbyvel@yahoo.com.ar>
2022-05-27 16:34:41 -0300

    Entrega TP

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const usernameEl = document.querySelector('#username');
 const emailEl = document.querySelector('#email');
-
-
 const form = document.querySelector('#signup');
 
 const checkUsername = () => {
@@ -24,8 +22,6 @@ const checkUsername = () => {
     return valid;
 };
 
-
-
 const checkEmail = () => {
     let valid = false;
     const email = emailEl.value.trim();
@@ -40,77 +36,62 @@ const checkEmail = () => {
     return valid;
 };
 
-
-
 const isEmailValid = (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 };
-
 
 const isRequired = value => value === '' ? false : true;
 const isBetween = (length, min, max) => length < min || length > max ? false : true;
 
 
 const showError = (input, message) => {
-    // get the form-field element
+
     const formField = input.parentElement;
-    // add the error class
+
     formField.classList.remove('success');
     formField.classList.add('error');
 
-    // show the error message
     const error = formField.querySelector('small');
     error.textContent = message;
 };
 
 const showSuccess = (input) => {
-    // get the form-field element
     const formField = input.parentElement;
 
-    // remove the error class
     formField.classList.remove('error');
     formField.classList.add('success');
 
-    // hide the error message
     const error = formField.querySelector('small');
     error.textContent = '';
 }
 
 form.addEventListener('submit', function (e) {
-    // prevent the form from submitting
     e.preventDefault();
 
-    // validate forms
     let isUsernameValid = checkUsername(),
         isEmailValid = checkEmail();
        
-
     let isFormValid = isUsernameValid &&
         isEmailValid;
       
-
-    // submit to the server if the form is valid
     if (isFormValid) {
         form.submit();
-        
-    }
+     }
 });
 
 const debounce = (fn, delay = 500) => {
     let timeoutId;
     return (...args) => {
-        // cancel the previous timer
         if (timeoutId) {
             clearTimeout(timeoutId);
         }
-        // setup a new timer
         timeoutId = setTimeout(() => {
             fn.apply(null, args)
         }, delay);
     };
 };
-// 
+
 form.addEventListener('input',function (e) {
     switch (e.target.id) {
         case 'username':
@@ -119,6 +100,5 @@ form.addEventListener('input',function (e) {
         case 'email':
             checkEmail();
             break;
-        
     }
 });
